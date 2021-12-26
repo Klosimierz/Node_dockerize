e8f1281bbedf13a673e97c90f411da3f44503a65
Klosimierz <klosimierz@gmail.com>
2021-12-26 22:46:33 +0100

    Fixed connection string

--- index.js
+++ index.js
@@ -9,15 +9,10 @@ const swapi_routes = require('./routes/swapi');
 const special_routes = require('./routes/special');
 
 const port = config.get('port');
-const conString = config.get('db_connection_string');
 
 const restrictRouting = require('./middleware/isAuth');
-/*
+
 mongoose.connect('mongodb://mongo:27017/nodebase')
-    .then(()=>{console.log('Connection established')})
-    .catch((err)=>{console.log(`Connection failed: ${err}`)});
-*/
-mongoose.connect('mongodb://localhost/testingDbFinal')
     .then(()=>{console.log('Connection established')})
     .catch((err)=>{console.log(`Connection failed: ${err}`)});
 
